ba3dd1be637a76982b46b7c4d927678ce8983c2e
tukasa <497116162@qq.com>
2022-07-20 14:37:59 +0800

    xxxx

--- src/views/zf/index.vue
+++ src/views/zf/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>zf</div>
+  <div class="zf-page">
+    <!-- 搜索框 -->
+    <div class="search-box">
+      <van-search
+        v-model="searchIpt"
+        label="广州"
+        input-align="left"
+        placeholder="  请输入小区或地址"
+        background="rgba(0,0,0,0)"
+      >
+        <template #left-icon>
+          <van-icon name="arrow-down" />
+        </template>
+      </van-search>
+      <div class="icon-map">
+        <van-icon name="aim" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
